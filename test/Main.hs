a5a5cbf65d407b2c99b8990f38f6023adf65344a
Potato Hatsue <1793913507@qq.com>
2025-08-26 00:06:31 -0700

    Update version bounds and add retry in test

--- test/Main.hs
+++ test/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Control.Exception (SomeException, catch)
 import Network.HTTP.Client (Manager)
 import Network.HTTP.Client.TLS (newTlsManager)
 import Test.HUnit
@@ -20,9 +21,14 @@ tests manager =
       createTest "getAurInfo" manager $ getAurInfo ["arch-hs-git"]
     ]
 
-createTest :: HasBaseUrl k => String -> Manager -> APIClient k a -> Test
+createTest :: (HasBaseUrl k) => String -> Manager -> APIClient k a -> Test
 createTest tag manager action = TestLabel tag . TestCase $ do
-  result <- runAPIClient manager action
+  -- Retry up to 3 times in case of network errors or rate limiting
+  result <- retry 3 $ runAPIClient manager action
   case result of
     Right _ -> pure ()
     Left err -> assertFailure $ show err
+
+retry :: Int -> IO a -> IO a
+retry 0 action = action
+retry n action = action `catch` \(_ :: SomeException) -> retry (n - 1) action
